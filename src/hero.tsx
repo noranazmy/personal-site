765e114f3bf18fb494c43fbb5aab490adafcda60
Noran Azmy <noran.azmy@gmail.com>
2025-08-24 09:26:35 +0100

    Make font sizes relative

--- src/hero.tsx
+++ src/hero.tsx
@@ -5,13 +5,15 @@ export function Hero() {
     <section id="hero" className="hero heading-and-content">
       <div className="heading">
         <img src="/Photo.jpg" alt="Personal photo" className="hero-image" />
-        <div className="display-2">
-          Software engineer and AI ethics researcher
+        <div className="hero-text">
+          <div className="display-2">
+            Software engineer and AI ethics researcher
+          </div>
+          <p className="paragraph-largest">
+            I am a senior full-stack software engineer with a focus on
+            product engineering, UI/UX design, tech ethics, and politics.
+          </p>
         </div>
-        <p className="paragraph-largest">
-          I am a senior full-stack software engineer.
-          My focus is on product engineering, UI/UX design, tech ethics, and politics.
-        </p>
         <a className="button primary resume-button" href="/Resume.pdf" target="_blank">
           <span className="material-symbols-outlined button-icon">
             download
